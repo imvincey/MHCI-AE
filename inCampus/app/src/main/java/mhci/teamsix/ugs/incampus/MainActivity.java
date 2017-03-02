753a6db66b3564be888a8a7514d1488d000dc3ea
Vince <limbingshun@gmail.com>
2017-03-02 21:26:34 +0800

    Saving for nav bar

--- inCampus/app/src/main/java/mhci/teamsix/ugs/incampus/MainActivity.java
+++ inCampus/app/src/main/java/mhci/teamsix/ugs/incampus/MainActivity.java
@@ -1,19 +1,20 @@
 package mhci.teamsix.ugs.incampus;
 
+import android.os.Bundle;
 import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -74,7 +75,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -86,9 +87,6 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        /*if (id == R.id.action_settings) {
-            return true;
-        }*/
 
         return super.onOptionsItemSelected(item);
     }
@@ -100,10 +98,10 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_event) {
-            // Handle the camera action
-        } else if (id == R.id.nav_coupon) {
 
-        } else if (id == R.id.nav_setting) {
+        } else if (id == R.id.nav_coupons) {
+
+        } else if (id == R.id.nav_settings) {
 
         } else if (id == R.id.nav_logout) {
             logout();
